fb955ba5a6c04655cfea166ff1ea80d15730d021
pavankalyan460 <aallakalyan20@gmail.com>
2024-12-05 00:15:43 +0530

    assign

---
+++ index.js
@@ -0,0 +1,9 @@
+let car = {
+    brand: "Toyota",
+    model: "Corolla",
+    year: 2020
+  };
+  
+  console.log(car.brand);  // "Toyota"
+  console.log(car.model);  // "Corolla"
+  console.log(car.year);   // 2020
